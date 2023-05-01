9861caea08eb144d370e58aabc563248fd549c3c
Samarth Hapse <131296047+samarthhapse@users.noreply.github.com>
2023-05-01 22:46:15 +0530

    Add files via upload

---
+++ agecheck.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+public class agecheck {
+        public static void main(String args[]){
+            Scanner sc = new Scanner(System.in);
+        System.out.print("Enter age = ");
+        int a= sc.nextInt();
+        //using float
+        if(a>18){
+            System.out.println("adult");
+        }
+        else{
+            System.out.println("not adult");
+        }
+        }
+    }
+        
+
