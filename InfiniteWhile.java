de27a2525248f2016b2ffd6a766f4c07c0d65bf5
Samarth Hapse <131296047+samarthhapse@users.noreply.github.com>
2023-05-05 02:28:01 +0530

    Add files via upload

---
+++ InfiniteWhile.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class InfiniteWhile {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+while(true){
+    System.out.println(" Bonjour !");
+}
+}
