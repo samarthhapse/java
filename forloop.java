226e1b07e99101f1d33c937dfb6a7fe495eedde2
Samarth Hapse <131296047+samarthhapse@users.noreply.github.com>
2023-05-05 02:21:06 +0530

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class forloop{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter any number = ");
+int n=sc.nextInt();
+System.out.println("Table is");
+for(int i=1;i<=10;i++){
+    System.out.println(n*i);
+
+}
+
+}
+}
