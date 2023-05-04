c3121fc24506cf5b40d6cb580296bd84ca444ef9
Samarth Hapse <131296047+samarthhapse@users.noreply.github.com>
2023-05-05 02:21:33 +0530

    Add files via upload

---
+++ WhileAndDowhile.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class WhileAndDowhile {
+    public static void main(String args[]){
+        int x=10;
+        //While loop is ENTRY CONTOLLED
+        //here loop executes only if contition satisfies
+        while(x<9){
+            System.out.println(" HOLA  ! ");
+        }
+        //do while loop is EXIT CONTROLLED
+        //here loop executes for 1 time for sure
+        do{
+            System.out.println(" Hii !!");
+        }while(x<9);
+    }
+}
