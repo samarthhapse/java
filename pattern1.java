3c626358259757532d6ded8e20b4f3ed9cba91ca
Samarth Hapse <131296047+samarthhapse@users.noreply.github.com>
2023-05-09 22:43:05 +0530

    Add files via upload

---
+++ pattern1.java
@@ -0,0 +1,16 @@
+//for(initialisation ; condition ; updation){       }
+// jitni bar condition satisfy hogi utni bar block execute hoga 
+import java.util.*;
+public class pattern1 {
+    public static void main(String args[]){
+        //solid rectangle problem
+        int n=4;
+        int m=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+    }
+}
