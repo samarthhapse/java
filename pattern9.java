52a3d64644e96e29131b5d7ce9a4389a171c9f72
Samarth Hapse <131296047+samarthhapse@users.noreply.github.com>
2023-05-11 20:56:23 +0530

    Add files via upload

---
+++ pattern9.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class pattern9 {
+    public static void main(String args[]){
+  //print numbers in half pyramid of reverse numbers
+        int n=5;
+        int m=5;
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+            System.out.print(i);
+        }
+        System.out.println();
+    }
+    }
+}
