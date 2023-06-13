b89449b0318044e0f686434051231c246886d841
Samarth Hapse <131296047+samarthhapse@users.noreply.github.com>
2023-06-13 21:57:28 +0530

    Add files via upload

---
+++ infinitedowhile.java
@@ -0,0 +1,15 @@
+//Write an infinite loop using do while condition.
+import java.util.*;
+
+public class infinitedowhile {
+    public static boolean infinite(){
+        do
+        {
+            System.out.println("ewwww");
+        }
+        while(true);
+    }
+    public static void main(String args[]){
+        infinite();
+    }
+}
