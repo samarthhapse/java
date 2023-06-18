8feb1899974cff9c3458a27e0c19540169168edb
Samarth Hapse <131296047+samarthhapse@users.noreply.github.com>
2023-06-18 23:14:23 +0530

    Add files via upload

---
+++ advpattern4.java
@@ -0,0 +1,20 @@
+public class advpattern4 {
+    public static void main(String args[]){
+        int n=5;
+        for(int i=1;i<=n;i++){
+            // spaces
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            // first half
+            for(int j=i;j>=1;j--){
+                System.out.print(j);
+            }
+            // second half
+            for(int j=2;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
